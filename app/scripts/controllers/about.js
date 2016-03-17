fd21e7a9763c174998eaa791171ff2777851beac
Ankit Muchhala <muchhalaankit@gmail.com>
2016-03-18 03:47:03 +0530

    scaffolding files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name daiictSenTeam13App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the daiictSenTeam13App
+ */
+angular.module('daiictSenTeam13App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
