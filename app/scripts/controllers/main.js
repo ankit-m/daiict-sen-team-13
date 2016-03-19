7d4e3f132ef1e740d00e34d0d74bdd7e5c04e277
Ankit Muchhala <muchhalaankit@gmail.com>
2016-03-20 02:37:39 +0530

    login ui and connection

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,22 @@
-'use strict';
+(function() {
+  'use strict';
 
-/**
- * @ngdoc function
- * @name daiictSenTeam13App.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the daiictSenTeam13App
- */
-angular.module('daiictSenTeam13App')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  /**
+   * @ngdoc function
+   * @name daiictSenTeam13App.controller:MainCtrl
+   * @description
+   * # MainCtrl
+   * Controller of the daiictSenTeam13App
+   */
+  angular.module('daiictSenTeam13App')
+    .controller('MainCtrl', ['$scope', function($scope) {
+      $scope.email = '';
+      $scope.password = '';
+      //  = submit; /*Self and this function used before definition*/
+
+      $scope.submit = function() {
+        console.log($scope.email);
+        console.log($scope.password);
+      };
+    }]);
+})();
