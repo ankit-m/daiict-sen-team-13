380687de853bdf601a940cc49cb5b86f3bd98101
Ankit Muchhala <muchhalaankit@gmail.com>
2016-03-20 03:21:55 +0530

    connect firebase for login

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,13 +10,24 @@
    */
   angular.module('daiictSenTeam13App')
     .controller('MainCtrl', ['$scope', function($scope) {
+      var ref = new Firebase('https://sfip.firebaseio.com/');
+
       $scope.email = '';
       $scope.password = '';
       //  = submit; /*Self and this function used before definition*/
 
       $scope.submit = function() {
-        console.log($scope.email);
-        console.log($scope.password);
+        console.log('submit form');
+        ref.createUser({
+          email: $scope.email,
+          password: $scope.password
+        }, function(error, userData) {
+          if (error) {
+            console.log("Error creating user:", error);
+          } else {
+            console.log("Successfully created user account with uid:", userData.uid);
+          }
+        });
       };
     }]);
 })();
