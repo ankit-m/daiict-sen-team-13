17226d5cfc7d2fb3b40d60aea6319e3c222002cc
Ankit Muchhala <muchhalaankit@gmail.com>
2016-04-09 03:51:34 +0530

    change link heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function(grunt) {
       },
       heroku: {
         options: {
-          remote: 'git@heroku.com:sfip.git',
+          remote: 'git@heroku.com:shrouded-bayou-89634.git',
           branch: 'master',
         }
       }
